cd2d756f8dd7bc450e49721733b84df92bc6edbc
QuentinZoli <quentin_liso@live.fr>
2025-11-25 17:54:40 +0100

    74-79

---
+++ 72_CommandLineArguments/command-line-arguments.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	argsWithProg := os.Args
+	fmt.Println(argsWithProg)
+
+	argsWithoutProg := os.Args[1:]
+	fmt.Println(argsWithoutProg)
+
+	arg := os.Args[3]
+	fmt.Println(arg)
+
+}
